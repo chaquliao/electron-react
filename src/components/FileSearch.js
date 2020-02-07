a36bf916d1a21be020c431e04e35ebdd95305104
廖方正清 <liaofangzhengqing@192.168.0.100>
2020-02-07 14:07:27 +0800

    增加配置

---
+++ src/components/FileSearch.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const FileSearch = ({ title, onFileSearch }) => {
+  const [inputActive, setInputActive] = useState(false);
+  const [value, setValue] = useState("");
+
+  return (
+      <div>
+          
+      </div>
+  )
+};
